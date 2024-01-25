1998321cf8654042c7d5abb2dae83f43db1add51
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2024-01-25 10:20:36 +0200

    finished search 2

--- search/lb_4_2.cpp
+++ search/lb_4_2.cpp
@@ -12,13 +12,15 @@ int main()
     int sequence[3] = {0, 0, 0};
     int arr_1[10] = {1, 0, 0, 3, 4, 6, 8, 5, 4, 99};
     int arr_2[10] = {1, 0, 0, 0, 4, 6, 8, 5, 4, 99};
-    int arr_3[100];
-    fill_array(arr_3, 100);
-    cout << search_sequence(arr_1, 10, sequence, 3) << endl;
-    cout << search_sequence(arr_2, 10, sequence, 3) << endl;
-    cout << "big array:\n";
-    print_array(arr_3, 100);
-    cout << search_sequence(arr_3, 100, sequence, 3) << endl;
+    int r_a_s = 100; // random array size
+    int arr_3[r_a_s];
+    fill_array(arr_3, r_a_s);
+    cout << boolalpha;
+    cout << "Found in arr_1: "<< search_sequence(arr_1, 10, sequence, 3) << endl;
+    cout << "Found in arr_2: "<< search_sequence(arr_2, 10, sequence, 3) << endl;
+    cout << "Random array:\n";
+    print_array(arr_3, r_a_s);
+    cout << "Found in random array: "<< search_sequence(arr_3, r_a_s, sequence, 3) << endl;
 }
 
 void fill_array(int *array, int lenght)
@@ -26,7 +28,7 @@ void fill_array(int *array, int lenght)
     srand((unsigned)(time(NULL)));
     for (int i = 0; i < lenght; i++)
     {
-        array[i] = rand() % 3;
+        array[i] = rand() % 5;
     }
 }
 void print_array(int *array, int lenght)
@@ -54,6 +56,7 @@ bool search_sequence(int *array, int n, int *sequence, int n_s)
             }
             if (result)
             {
+                cout << "Position = " << i << " ";
                 return result;
             }
         }
